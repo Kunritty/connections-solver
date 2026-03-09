8261e3dceeae7dfbf7e1522fd42862d875ac40c4
Kunritty <kunrittyh06@gmail.com>
2026-03-09 16:58:22 -0700

    Fixed train_test_split function

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -1,3 +1,7 @@
+from data_loader.train_test_split import train_test_split
+from data_loader.loader import load_huggingface_dataset
+
 __all__ = [
-    
+    "load_huggingface_dataset",
+    "train_test_split"
 ]
--- data_loader/train_test_split.py
+++ data_loader/train_test_split.py
@@ -1,4 +1,4 @@
-from data_loader import load_huggingface_dataset
+from data_loader.loader import load_huggingface_dataset
 
 
 def train_test_split(test_size=0.2,seed=175):
@@ -6,6 +6,7 @@ def train_test_split(test_size=0.2,seed=175):
     Returns ds_train, ds_test splits from Hugging Face
     """
     ds = load_huggingface_dataset()
+    ds = ds["train"]
     split = ds.train_test_split(test_size=test_size, seed=seed)
     ds_train = split["train"]
     ds_test = split["test"]
