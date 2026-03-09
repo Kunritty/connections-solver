956897e0d941425fd4afcddbec517e94b14ad9a2
Kunritty <kunrittyh06@gmail.com>
2026-03-09 16:15:08 -0700

    Added train_test_split function

--- data_loader/train_test_split.py
+++ data_loader/train_test_split.py
@@ -1,2 +1,11 @@
-def train_test_split():
-    pass
+from conn import load_connections_from_hf
+
+def train_test_split(test_size=0.2,seed=175):
+    """
+    Returns ds_train, ds_test splits from Hugging Face
+    """
+    ds = load_connections_from_hf()
+    split = ds.train_test_split(test_size=test_size, seed=seed)
+    ds_train = split["train"]
+    ds_test = split["test"]
+    return ds_train, ds_test
